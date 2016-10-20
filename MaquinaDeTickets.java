82d968c6b7a952a88f45dbd21aa04c2c4abb1473
naxoozil <nachoviuda@gmail.com>
2016-10-20 12:04:50 +0200

    traducido codigo al español

--- MaquinaDeTickets.java
+++ MaquinaDeTickets.java
@@ -9,12 +9,12 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2011.07.31
  */
-public class TicketMachine
+public class MaquinaDeTickets
 {
     // The price of a ticket from this machine.
-    private int price;
+    private int precio;
     // The amount of money entered by a customer so far.
-    private int balance;
+    private int dineroIntroducido;
     // The total amount of money collected by this machine.
     private int total;
 
@@ -23,36 +23,36 @@ public class TicketMachine
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
      */
-    public TicketMachine(int cost)
+    public MaquinaDeTickets(int coste)
     {
-        price = cost;
-        balance = 0;
+        precio = coste;
+        dineroIntroducido = 0;
         total = 0;
     }
 
     /**
      * Return the price of a ticket.
      */
-    public int getPrice()
+    public int obtenerPrecio()
     {
-        return price;
+        return precio;
     }
 
     /**
      * Return the amount of money already inserted for the
      * next ticket.
      */
-    public int getBalance()
+    public int obtenerDineroIntroducido()
     {
-        return balance;
+        return dineroIntroducido;
     }
 
     /**
      * Receive an amount of money from a customer.
      */
-    public void insertMoney(int amount)
+    public void introducirDinero(int cantidad)
     {
-        balance = balance + amount;
+        dineroIntroducido = dineroIntroducido + cantidad;
     }
 
     /**
@@ -60,19 +60,19 @@ public class TicketMachine
      * Update the total collected and
      * reduce the balance to zero.
      */
-    public void printTicket()
+    public void imprimirTicket()
     {
         // Simulate the printing of a ticket.
         System.out.println("##################");
         System.out.println("# The BlueJ Line");
         System.out.println("# Ticket");
-        System.out.println("# " + price + " cents.");
+        System.out.println("# " + precio + " centimos.");
         System.out.println("##################");
         System.out.println();
 
         // Update the total collected with the balance.
-        total = total + balance;
+        total = total + dineroIntroducido;
         // Clear the balance.
-        balance = 0;
+        dineroIntroducido = 0;
     }
 }
